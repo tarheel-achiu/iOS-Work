8b5118aba86cd07f0f625fcdb3bd6fd368462c4d
Andy Chiu <achiu93@live.com>
2017-05-20 16:54:44 -0700

    Initial Commit

---
+++ iOS course/iOS course/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS course
+//
+//  Created by Andy Chiu on 5/20/17.
+//  Copyright © 2017 Andy Chiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS course/iOS course/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS course
+//
+//  Created by Andy Chiu on 5/20/17.
+//  Copyright © 2017 Andy Chiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
